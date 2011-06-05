053e3938aaa98008e533b796766319d5a8230635
Bruno Gola <brunogola@gmail.com>
2011-06-04 22:46:20 -0300

    email

--- app/volunteers/models.py
+++ app/volunteers/models.py
@@ -5,6 +5,7 @@ from google.appengine.ext import db
 class Profile(db.Model):
     user = db.UserProperty()
     name = db.StringProperty()
+    email = db.EmailProperty()
     mobile = db.PhoneNumberProperty()
     location = db.GeoPtProperty()
     radius = db.IntegerProperty()
