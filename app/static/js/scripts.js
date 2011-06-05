58b198895d986eb99188617a37d3e5768379d003
Bruno Gola <brunogola@gmail.com>
2011-06-05 00:58:02 -0300

    merge layout novo

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,18 +1,70 @@
 var d = document;
 
-function validate_form_register() {
-	var txtNome = d.getElementById('txtNome').value;
-	var txtEmail = d.getElementById('txtEmail').value;
-	var txtDDD = d.getElementById('txtDDD').value;
-	var txtTelefone = d.getElementById('txtTelefone').value;
-	
-	if (txtNome == "" || txtEmail == "" || txtDDD == "" || txtTelefone == "") {
-		d.getElementById('warning').style.visibility = "visible";
-		d.getElementById('warning').innerHTML = "Favor preencher todos os campos!";
-
-	}
-	else {
-		d.getElementById('formCadastro').submit();
-		d.getElementById('warning').style.visibility = "hidden";
-	}
+var geocoder;
+var map;
+var marker;
+   
+function initialize(){
+//MAP
+  var latlng = new google.maps.LatLng(41.659,-4.714);
+  var options = {
+    zoom: 16,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
+       
+  map = new google.maps.Map(document.getElementById("mymap"), options);
+       
+  //GEOCODER
+  geocoder = new google.maps.Geocoder();
+       
+  marker = new google.maps.Marker({
+    map: map,
+    draggable: true
+  });
+               
 }
+
+$(document).ready(function() { 
+         
+  initialize();
+				  
+  $(function() {
+    $("#endereco").autocomplete({
+      //This bit uses the geocoder to fetch address values
+      source: function(request, response) {
+        geocoder.geocode( {'address': request.term }, function(results, status) {
+          response($.map(results, function(item) {
+            return {
+              label:  item.formatted_address,
+              value: item.formatted_address,
+              latitude: item.geometry.location.lat(),
+              longitude: item.geometry.location.lng()
+            }
+          }));
+        })
+      },
+      //This bit is executed upon selection of an address
+      select: function(event, ui) {
+        $("#location").val(ui.item.latitude + ',' + ui.item.longitude);
+        var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+        marker.setPosition(location);
+        map.setCenter(location);
+      }
+    });
+  });
+	
+  //Add listener to marker for reverse geocoding
+  google.maps.event.addListener(marker, 'drag', function() {
+    geocoder.geocode({'latLng': marker.getPosition()}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[0]) {
+          $('#endereco').val(results[0].formatted_address);
+          $('#latitude').val(marker.getPosition().lat());
+          $('#longitude').val(marker.getPosition().lng());
+        }
+      }
+    });
+  });
+  
+});
