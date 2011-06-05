e5569420638d0054534740439e8f2e2e1440a410
Ricardo Bánffy <rbanffy@gmail.com>
2011-06-05 07:42:37 -0300

    admin minimo

--- app/volunteers/admin_handlers.py
+++ app/volunteers/admin_handlers.py
@@ -15,14 +15,21 @@ from tipfyext.jinja2 import Jinja2Mixin
 from werkzeug import cached_property
 from wtforms.form import WebobInputWrapper
 
-from forms import ProfileForm
+from admin_forms import *
 from models import *
+from utils import *
 
 from pymaps import PyMap, Map
 
+
 class SkillsListHandler(RequestHandler, Jinja2Mixin):
     def get(self):
-        raise NotImplementedError
+        skills = Skill.all()
+        return self.render_response('simple_list_admin.html', 
+                                    l = skills, 
+                                    recent_actions = recent_actions(), 
+                                    top_skills = top_skills(), 
+                                    top_resources = top_resources()) 
 
 class SkillsAdminHandler(RequestHandler, Jinja2Mixin):
     def get(self):
@@ -31,8 +38,50 @@ class SkillsAdminHandler(RequestHandler, Jinja2Mixin):
 class ResourceListHandler(RequestHandler, Jinja2Mixin):
     def get(self):
         resources = Resource.all()
-        return self.render_response('resource_admin.html', l = resources) 
+        return self.render_response('simple_list_admin.html', 
+                                    l = resources, 
+                                    recent_actions = recent_actions(), 
+                                    top_skills = top_skills(), 
+                                    top_resources = top_resources()) 
 
 class ResourceAdminHandler(RequestHandler, Jinja2Mixin):
     def get(self):
         raise NotImplementedError
+
+class NewSkillHandler(RequestHandler, Jinja2Mixin):
+    def get(self):
+        return self.render_response('simple_form.html', 
+                                    form = self.form, 
+                                    recent_actions = recent_actions(), 
+                                    label = u'Adicionar nova capacitacao', 
+                                    top_skills = top_skills(), 
+                                    top_resources = top_resources()) 
+
+    def post(self, **kwargs):
+        if self.form.validate():
+            Skill(name = self.form.name.data).put()
+            return redirect('/admin/skills/')
+        return self.get(**kwargs)
+
+    @cached_property
+    def form(self):
+        return SkillForm(self.request.form)
+
+class NewResourceHandler(RequestHandler, Jinja2Mixin):
+    def get(self):
+        return self.render_response('simple_form.html', 
+                                    form = self.form, 
+                                    recent_actions = recent_actions(), 
+                                    label = u'Adicionar novo recurso', 
+                                    top_skills = top_skills(), 
+                                    top_resources = top_resources()) 
+
+    def post(self, **kwargs):
+        if self.form.validate():
+            Resource(name = self.form.name.data).put()
+            return redirect('/admin/resources/')
+        return self.get(**kwargs)
+
+    @cached_property
+    def form(self):
+        return ResourceForm(self.request.form)
