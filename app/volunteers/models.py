3254c3b8eae15a2abde048c83199f57e2068088d
Ricardo Bánffy <rbanffy@gmail.com>
2011-06-05 01:18:06 -0300

    adicionando modelos

--- app/volunteers/models.py
+++ app/volunteers/models.py
@@ -1,6 +1,13 @@
 # coding: utf-8
 
 from google.appengine.ext import db
+from django.template import defaultfilters
+
+class Skill(db.Model):
+    name = db.StringProperty()
+    
+    def slug(self):
+        return defaultfilters.slugify(self.name)
 
 class Profile(db.Model):
     user = db.UserProperty()
@@ -9,3 +16,10 @@ class Profile(db.Model):
     mobile = db.PhoneNumberProperty()
     location = db.GeoPtProperty()
 
+class Profile_Skill(db.Model):
+    profile = db.ReferenceProperty(Profile)
+    skill = db.ReferenceProperty(Skill)
+
+class Action(db.Model):
+    when = db.DateTimeProperty(auto_now_add = True)
+    what = db.StringProperty()
