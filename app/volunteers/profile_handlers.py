12d8f09bf66154025d781613c55b1001a0978c48
Ricardo Bánffy <rbanffy@gmail.com>
2011-06-05 05:21:32 -0300

    resources

---
+++ app/volunteers/profile_handlers.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+    hello_world.handlers
+    ~~~~~~~~~~~~~~~~~~~~
+
+    Hello, World!: the simplest tipfy app.
+
+    :copyright: 2009 by tipfy.org.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from tipfy.app import Response, redirect
+from tipfy.handler import RequestHandler
+from tipfyext.jinja2 import Jinja2Mixin
+from werkzeug import cached_property
+from wtforms.form import WebobInputWrapper
+
+from forms import ProfileForm
+from models import *
+
+from pymaps import PyMap, Map
+
+class VolunteersMainHandler(RequestHandler, Jinja2Mixin):
+    def get(self):
+        """Simply returns a Response object with an enigmatic salutation."""
+        recent_actions = [ {'when': a.when, 
+                            'name':a.name, 
+                            'what': a.what } 
+                           for a in Action.all().order('-when').fetch(10) ]
+        return self.render_response('index.html', form = self.form, recent_actions = recent_actions)
+
+    def post(self, **kwargs):
+        if self.form.validate():
+            profile = Profile(
+                name = self.form.name.data,
+                mobile = self.form.mobile.data,
+                location = self.form.location.data,
+                email = self.form.email.data,
+            )
+            profile.put()
+            # TODO: Enviar um e-mail com um link para autenticação
+            Action(who = profile,
+                   what = 'tornou-se membro do rhok bush!',
+                   location = self.form.location.data).put()
+            return redirect('/')
+        return self.get(**kwargs)
+
+    @cached_property
+    def form(self):
+        return ProfileForm(self.request.form)
+
+class MapMainHandler(RequestHandler, Jinja2Mixin):
+    def get(self):
+        points = [ (profile.location.lat, profile.location.lon, profile.name, '') for profile in Profile.all().fetch(1000) ]
+        pm = PyMap(maplist=[Map("mapao", points)])
+        return self.render_response('mapa.html', pymap=pm)
+
+
+class AboutHandler(RequestHandler, Jinja2Mixin):
+    def get(self):
+        recent_actions = [ {'when': a.when, 
+                            'name':a.name, 
+                            'what': a.what} 
+                           for a in Action.all().order('-when').fetch(10) ]
+        return self.render_response('about.html', recent_actions = recent_actions)
